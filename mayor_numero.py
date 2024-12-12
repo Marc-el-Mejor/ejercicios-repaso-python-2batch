a5d59763b3acbfe6bbb368e3c5fd0af101b49b61
Marc-el-Mejor <mplaporcar@gmail.com>
2024-12-12 13:45:38 +0100

    borados dos espacios

--- mayor_numero.py
+++ mayor_numero.py
@@ -1,6 +1,5 @@
 i=5
 lista=[]
-
 while i>0:
     try:
         numero=int(input("Dime un numero entero: "))
@@ -8,7 +7,6 @@ while i>0:
         i-=1
     except:
         print("No es un numero Gilipollas")
-
 numero_mayor=0
 for x in lista:
     if numero_mayor>=x:
